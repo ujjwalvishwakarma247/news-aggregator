0239c024f9f3a171e66b234b58a8f6d0d3de4997
ujjwal <vishwakarmaujjwal247@gmail.com>
2025-11-09 17:25:07 +0530

    ha

--- src/main/java/com/example/news/NewsApplication.java
+++ src/main/java/com/example/news/NewsApplication.java
@@ -2,10 +2,9 @@ package com.example.news;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
-@EnableScheduling
 public class NewsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/news/RssfeedService.java
+++ src/main/java/com/example/news/RssfeedService.java
@@ -7,7 +7,6 @@ import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -23,8 +22,7 @@ public  class RssfeedService {
     @Autowired
     private Repositorynews repo;
 
-    //  This method will run every 10 minutes automatically
-    @Scheduled(fixedRate = 600000) // 10 minutes = 600000 milliseconds
+  
     public List<NewsItems> fetchNewFromRss(String rssUrl){
         List<NewsItems> newsList = new ArrayList<>();
         try {
